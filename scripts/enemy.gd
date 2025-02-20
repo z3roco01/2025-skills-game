69da1e5c3e42ce49871261e05f768e31d430564d
z3roco01 <zerocool@e.email>
2025-02-19 20:45:42 -0600

    basic ememyu

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,13 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@export var SPEED = 4
+var vel = Vector2.ZERO
+# get the player from the parent
+@onready var player = get_parent().get_node("./player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var collision = move_and_collide(position.direction_to(player.position) * SPEED)
+	
+	if(collision and collision.get_collider() == player):
+		player.decrementHealth(1)
