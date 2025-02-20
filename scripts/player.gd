5c3dba17a840f33aea04b62cfc3c0e6fae8dac01
droooids <thebeasts611@gmail.com>
2025-02-19 18:09:04 -0600

    implement dash  ඞ
    wow!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,61 @@
 extends CharacterBody2D
 
-const SPEED = 700.0
+var startHealth = 100
+var currentHealth
+
+const DEFAULT_SPEED = 700.0
+const DASH_SPEED = 2000
+
+var speed = DEFAULT_SPEED
+var dashOnCD = false
+
 # gets the main node, which contains the players preferences
 @onready var mainNode = get_node("/root/main")
+@onready var healthLabel = get_node("/root/main/testworld/health")
+@onready var dashTimer = $dashTimer
+@onready var dashCooldown = $dashCooldown
 
 func _ready() -> void:
-	pass
-	#print(mainNode)
+	# set current health to the starting health
+	currentHealth = startHealth
+	
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "back")
 	
-	velocity = direction * SPEED
+	velocity = direction * speed
+	
+	# check for dash input and able to use
+	if(Input.is_action_just_pressed("dash") and dashCooldown.is_stopped()):
+		
+		# start dash
+		speed = DASH_SPEED
+		dashTimer.start()
+		
+		# start dash cooldown
+		dashCooldown.start()
+	
 	
 	move_and_slide()
+	
+	
+	
+
+func decrementHealth(amount:int):
+	
+	# check for death
+	if(currentHealth - amount > 0):
+		currentHealth -= amount
+	else:
+		currentHealth = 0
+		# TODO death stuff
+	
+	# update health :)
+	healthLabel.text = str(currentHealth)
+	
+	pass #ts pmo
+
+
+func _on_dash_timer_timeout() -> void:
+	# go back to normal speed
+	speed = DEFAULT_SPEED
