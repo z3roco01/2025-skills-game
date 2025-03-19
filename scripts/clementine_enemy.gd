c5af33c68ee958f1f68d81814ab84c9d46399240
zerocool <46464404+z3roco01@users.noreply.github.com>
2025-03-18 22:56:51 -0500

    unimplemented carmile defense

--- scripts/clementine_enemy.gd
+++ scripts/clementine_enemy.gd
@@ -1,1 +1,20 @@
 extends Enemy
+
+# the amount we will push the player from ( ONLY SET X )
+@export var push = Vector2(500, 0)
+
+func idleAction() -> void:
+	pass
+
+# run every physics tick when the attack cooldown is 0
+func attack() -> void:
+	# player is close
+	if(distanceToPlayer() <= 400):
+		var angle = push.angle_to(player.position)
+		print(rad_to_deg(angle))
+		var rotatedPush = push.rotated(push.angle_to(player.position))
+		
+		
+		print(rotatedPush)
+		#get_tree().create_tween().tween_property(player, "position", player.position + rotatedPush, 0.2)
+		attackCooldown = 200  
