3543ec1a4a25baf0431dc8fc14bf5a54c93a5507
zerocool <46464404+z3roco01@users.noreply.github.com>
2025-03-22 01:11:54 -0500

    HOLY FREAK IT WORKS

--- scripts/clementine_enemy.gd
+++ scripts/clementine_enemy.gd
@@ -1,7 +1,10 @@
 extends Enemy
 
-# the amount we will push the player from ( ONLY SET X )
-@export var push = Vector2(500, 0)
+# the amount we will push the player by when too close
+@export var push = 500
+
+# used to get the rotation for pushing
+@onready var pushRotator = $pushRotator
 
 func idleAction() -> void:
 	pass
@@ -10,11 +13,15 @@ func idleAction() -> void:
 func attack() -> void:
 	# player is close
 	if(distanceToPlayer() <= 400):
-		var angle = push.angle_to(player.position)
-		print(rad_to_deg(angle))
-		var rotatedPush = push.rotated(push.angle_to(player.position))
+		pushRotator.look_at(player.position)
+		var pushVec = Vector2.RIGHT.rotated(pushRotator.rotation) * push
+		get_tree().create_tween().tween_property(player, "position", player.position + pushVec, 0.4).set_trans(Tween.TRANS_BACK)
+		attackCooldown = 100
+		#var angle = push.angle_to(player.position)
+		#print(rad_to_deg(angle))
+		#var rotatedPush = push.rotated(push.angle_to(player.position))
 		
 		
-		print(rotatedPush)
+		#print(rotatedPush)
 		#get_tree().create_tween().tween_property(player, "position", player.position + rotatedPush, 0.2)
-		attackCooldown = 200  
+		#attackCooldown = 200  
