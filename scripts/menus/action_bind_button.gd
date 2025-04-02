000ab4bcb3595b972f1ac2a3e85b03d3eb26e2d3
zerocool <46464404+z3roco01@users.noreply.github.com>
2025-04-02 08:52:52 -0600

    working keybind options

---
+++ scripts/menus/action_bind_button.gd
@@ -0,0 +1,24 @@
+extends Button
+
+# the name of the action that will be set
+@export var actionName : String
+var gettingInput = false
+
+func _ready() -> void:
+	setText()
+
+func _pressed() -> void:
+	gettingInput = true
+	text = "Waiting..."
+
+func _input(event: InputEvent) -> void:
+	if(gettingInput):
+		InputMap.action_erase_events(actionName)
+		InputMap.action_add_event(actionName, event)
+		gettingInput = false
+		setText()
+
+func setText() -> void:
+	# set the text to the name of the first event for the action
+	# will only support one event per action
+	text = InputMap.action_get_events(actionName)[0].as_text()
