50e985c3b75824517f315314329cb94bcd3b1c74
droooids <thebeasts611@gmail.com>
2025-04-08 18:31:01 -0500

    fix volume sliders not loading the current value on open

--- scripts/menus/volume_slider.gd
+++ scripts/menus/volume_slider.gd
@@ -1,7 +1,14 @@
 extends Range
 
 @export var busName : String
+var idx
+
+func _ready() -> void:
+	# get idx of the bus target
+	idx =AudioServer.get_bus_index(busName)
+	# get current value of volume and set slider to that value
+	value = db_to_linear(AudioServer.get_bus_volume_db(idx))
 
 func _value_changed(new_value: float) -> void:
-	var idx = AudioServer.get_bus_index(busName)
+	# set new volume
 	AudioServer.set_bus_volume_db(idx, linear_to_db(value))
